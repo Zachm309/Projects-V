d9a48e60b4f264aae528e1523401ecf47ce59762
Zachm309 <124308184+Zachm309@users.noreply.github.com>
2024-10-05 18:40:56 -0400

    Update client.c
    while loop in wrong spot. Working C file now

--- serverCodeBs/client.c
+++ serverCodeBs/client.c
@@ -7,17 +7,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define SERVER_IP "192.168.1.226"
+#define SERVER_PORT 8080
+
 char buffer[BUFSIZ];
+
 
 int main(int argc, char *argv[]) {
     int client_socket, len;
     struct sockaddr_in server_addr;
-
+    
+while(1) {	
+	
     // Check for sanity
-    if (argc != 3) {
-        printf("usage: c IP_ADDRESS PORT_NUMBER\n");
-        return 1;
-    }
+   
 
     // Create socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -27,9 +30,9 @@ int main(int argc, char *argv[]) {
 
     // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(argv[2]));
+    server_addr.sin_port = htons(SERVER_PORT);
 
     // Attempt a connection to server
     if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
@@ -39,14 +42,14 @@ int main(int argc, char *argv[]) {
     }
 
     // Loop to keep client running
-    while (1) {
-        printf("Enter a command [date | who | df | exit]: ");
+        printf("Enter a command [stream | W(forward) | A(left) | D(Right) | S(Backward)]: ");
         fflush(stdout);
-     //      printf("Enter a command [date | who | df | exit]: ");
+     
+     	//printf("Enter a command [date | who | df | exit]: ");
         fgets(buffer, sizeof(buffer), stdin);
-        if (buffer[strlen(buffer) - 1] == '\n')
+        if (buffer[strlen(buffer) - 1] == '\n') {
             buffer[strlen(buffer) - 1] = '\0';
-
+	}
         // Check for exit command
         if (strcmp(buffer, "exit") == 0) {
             break; // Exit the loop
@@ -59,13 +62,11 @@ int main(int argc, char *argv[]) {
         len = read(client_socket, buffer, sizeof(buffer));
         buffer[len] = '\0'; // Null-terminate the response
         printf("Result of command:\n%s\n\n", buffer);
-    }
-
+}
     // Cleanup
     close(client_socket);
     printf("Client is finished\n");
 
     return 0;
 }
-
 
