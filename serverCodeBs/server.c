0b3170e17094b5ffc1202c67384b0356db25df35
Zachm309 <124308184+Zachm309@users.noreply.github.com>
2024-10-05 14:48:00 -0400

    Update server.c
    Updated server.C. File compiles in this state however can only accept one packet of commands from client in this state.
    Changes: Function for writing to serial port for STM communication
                     Launches python code for camera stream fine, but still bit receiving data from it

--- serverCodeBs/server.c
+++ serverCodeBs/server.c
@@ -1,9 +1,10 @@
-/*
- * server.c
- *
- * This is a sample internet server application that will respond
- * to requests on reconfigurable port 
- */
+
+ //server.c
+ 
+ //This is a sample internet server application that will respond
+ //to requests on reconfigurable port 
+ 
+ //Almost reciving data from python. Check buf SIZE
 
 #include <stdio.h>
 #include <string.h>
@@ -19,9 +20,18 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-//#define PORT 5000
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <errno.h>
+
+
+
+#define PORT 5000
+#define PYTHON_BUFFER 256
 
 char buffer[BUFSIZ];
+char python_output[PYTHON_BUFFER];
 
 
 /*
@@ -32,15 +42,70 @@ char buffer[BUFSIZ];
  * wait for the child to exit)
  */
 
-void
-SigCatcher (int n)
+void SigCatcher (int n)
 {
     wait3 (NULL, WNOHANG, NULL);    
 	signal (SIGCHLD, SigCatcher);
 }
 
-int
-main (int argc, char *argv[])
+int serial(const char *message) {
+	
+	int serial_port = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
+	if (serial_port < 0) {
+		printf("Error %i from open: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	
+	struct termios tty;
+	memset(&tty, 0, sizeof(tty));
+	
+	if (tcgetattr(serial_port, &tty) != 0) {
+		printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+		return -1;
+	}
+	
+	//settint flag
+	cfsetispeed(&tty, B9600);
+	cfsetospeed(&tty, B9600);
+	
+	tty.c_cflag &= ~PARENB; // NO parity
+	tty.c_cflag &= ~CSTOPB; // 1 bit stop
+	tty.c_cflag &= ~CSIZE; // Clear the size mask
+	tty.c_cflag |= ~CS8; // No parity
+	
+	tty.c_cflag &= ~CRTSCTS;
+	tty.c_cflag |= CREAD | CLOCAL;
+	
+	tty.c_lflag &= ~ICANON; //raw mode
+	tty.c_lflag &= ~(ECHO | ECHOE | ISIG);
+	tty.c_iflag &= ~(IXON | IXOFF | IXANY); // no software flow control
+	tty.c_oflag &= ~OPOST; //Raw 
+	
+	
+	tty.c_cc[VMIN] = 0;
+	tty.c_cc[VTIME] = 10; // 1 second time
+	
+	
+	
+	//
+	
+	if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+		printf("Error %i from tcsetattr: %s\n");
+		return -1;
+	}
+	
+	int n_written = write(serial_port, message, strlen(message));
+	if (n_written < 0) {
+		printf("Error %i from write: %s\n", errno, strerror(errno));
+	}
+	
+	close(serial_port);
+	return 0;
+}
+
+
+
+int main (int argc, char *argv[])
 {
         //my own server socket entity and alsso the client enitty im talking to
 	int server_socket, client_socket;
@@ -48,7 +113,6 @@ main (int argc, char *argv[])
 	struct sockaddr_in client_addr, server_addr; //tracks client and my own IP address
 	int len, i;
 	FILE *p;
-
         // ./server 8080
 	if (argc != 2) {
 		printf ("usage: c PORT_NUMBER\n");
@@ -105,6 +169,7 @@ main (int argc, char *argv[])
 	 */
 
 	while (1) {
+		
 		/*
 		 * accept a packet from the client
 		 */
@@ -143,55 +208,85 @@ main (int argc, char *argv[])
 			 * process command, and obtain outgoing data
 			 */
 
-			if (strcmp (buffer, "date") == 0) {
-				if (len = (p = popen ("date", "r")) != NULL) {
-					len = fread (buffer, 1, sizeof (buffer), p);
-					pclose (p);
+			//Chnage commands to what they actually do
+			if (strcmp (buffer, "W") == 0) {
+				strcpy (buffer, "forward\n");
+				len = strlen (buffer);
+				printf("Command:%s", buffer);
+			} else if (strcmp (buffer, "A") == 0) {
+				strcpy (buffer, "Left\n");
+				len = strlen (buffer);
+				printf("Command:%s", buffer);
+			} else if (strcmp (buffer, "S") == 0) {
+				strcpy (buffer, "Backwards\n");
+				len = strlen (buffer);
+				printf("Command:%s", buffer);
+			} else if (strcmp (buffer, "D") == 0) {
+				strcpy (buffer, "Right\n");
+				len = strlen (buffer);
+				printf("Command:%s", buffer);
+			} else if (strcmp (buffer, "stream") == 0) {
+				
+				//Execute python script and read its output
+				FILE *fp;
+				char command[BUFSIZ];
+				
+				//modify command to include full path to main.py
+				snprintf(command, sizeof(command), "python3 /home/zpi/tests/Projects-V/CV-Code/main.py");
+				
+				//open command for reading
+				fp = popen(command, "r");
+				if(fp == NULL) {
+					printf("Failed to run command");
 				} else {
-					strcpy (buffer, "Can't run date command\n");
-					len = strlen (buffer);
-				}	/* endif */
-			} else if (strcmp (buffer, "who") == 0) {
-				if (len = (p = popen ("who", "r")) != NULL) {
-					len = fread (buffer, 1, sizeof (buffer), p);
-					pclose (p);
-				} else {
-					strcpy (buffer, "Can't run who command\n");
-					len = strlen (buffer);
-				}	/* endif */
-			} else if (strcmp (buffer, "df") == 0) {
-				if (len = (p = popen ("df", "r")) != NULL) {
-					len = fread (buffer, 1, sizeof (buffer), p);
-					pclose (p);
-				} else {
-					strcpy (buffer, "Can't run df command\n");
-					len = strlen (buffer);
-				}	/* endif */
+					if(fgets(python_output, sizeof(python_output), fp) != NULL) {
+						python_output[strcspn(python_output, "\n")] = 0;
+						snprintf(buffer, BUFSIZ, "Direction from python: %s\n", python_output);
+						printf("Direction recived from python code: %s\n", python_output);
+					}
+					pclose(fp);
+				}
 			} else {
 				strcpy (buffer, "invalid command\n");
 				len = strlen (buffer);
+				
 			}	/* endif */
 
 			/*
 			 * write data to client, close socket, and exit child app
 			 */
-			
 			//write(client_socket, "Response  inbound", 18);
-			
-			write (client_socket, buffer, len);
-			close (client_socket);
+			write(client_socket, buffer, len);
+			close(client_socket);
 			return 0;
+			
+			
 		} else {
 			/*
 			 * this is done by parent ONLY
 			 */
-
-			close (client_socket);
+			close(client_socket);
 		}	/* endif */
 	}	/* end while */
-
 	return 0;
 }	/* end main */
+
+/*
+int launch_stream() {
+	if (chdir("/home/zpi/tests/Projects-V/CV-Code") !=0){
+			perror("chdir failed");
+			exit(1);
+		}
+		//execute the python script to stream camera
+	execlp("python3", "python3", "main.py", (char *)NULL);
+	perror("Error executing python script");
+	exit(1);
+	return 0;
+}
+*/
+
+
+
 
 
 
