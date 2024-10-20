88d96fbc06ddf4f21b1f5a798c270d7e6313ca0a
JesseRussell2240 <124098610+JesseRussell2240@users.noreply.github.com>
2024-10-20 10:16:49 -0400

    Update camTest.py

--- CV-Code/unitTesting/camTest.py
+++ CV-Code/unitTesting/camTest.py
@@ -1,12 +1,14 @@
+#unit test for cv2 and webcamera USB
+
 import cv2
 
-# Open the USB camera (0 is the index for the first camera)
+# this line helps us identify what camera when multiples are in use
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     if ret:
-        cv2.imshow('USB Camera Feed', frame)
+        cv2.imshow('USB Cam Feed', frame)
     
     # Exit the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
