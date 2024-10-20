032174f6ecbefebe195bfaea68cf22bff917e4ab
Zachm309 <124308184+Zachm309@users.noreply.github.com>
2024-10-20 09:37:55 -0300

    Add files via upload

--- CV-Code/ImageProcessing/temp/main.py
+++ CV-Code/ImageProcessing/temp/main.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,Response
+import cv2
+cap = cv2.VideoCapture(0)
 # Create an instance of the Flask class
+
 app = Flask(__name__)
+
+def generate_frames():
+    while True:
+
+        ## read the camera frame
+        success,frame=cap.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+        
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 # Define the route for the root URL
 @app.route('/')
-def hello_world():
+def main():
     return render_template('index.html')  # Render the HTML template
+    
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace: boundry=frame')
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)  # Run in debug mode to allow live reloading
+    app.run(debug=False)  # Run in debug mode to allow live reloading
