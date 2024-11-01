55c4274fd3a17ed01162f0c8d3468867c5a9ffc0
Zachm309 <124308184+Zachm309@users.noreply.github.com>
2024-11-01 13:04:31 -0400

    Add files via upload

--- serverCodeBs/client.c
+++ serverCodeBs/client.c
@@ -6,21 +6,29 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 #define SERVER_IP "192.168.1.226" //change this if working from home
 #define SERVER_PORT 8080
+#define TIMEOUT 10
+#define TIMEOUT_MSG "Keep_Alive"
 
 char buffer[BUFSIZ];
+
+void send_msg(int client_socket) {
+	write(client_socket, TIMEOUT_MSG, strlen(TIMEOUT_MSG));
+}
 
 
 int main(int argc, char *argv[]) {
     int client_socket, len;
     struct sockaddr_in server_addr;
-    
-while(1) {	
+    time_t last_command_time = time(NULL);
+    	
 	
     // Check for sanity
    
+
 
     // Create socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -41,8 +49,16 @@ while(1) {
         return 4;
     }
 
-    // Loop to keep client running
-        printf("Enter a command [stream | W(forward) | A(left) | D(Right) | S(Backward)]: ");
+
+    while(1) {
+    	
+    	
+    	if (difftime(time(NULL), last_command_time) >= TIMEOUT) {
+    		send_msg(client_socket);
+    		last_command_time = time(NULL);
+    	}
+    	
+        printf("Enter a command [ stream | W | A | D | S | exit ]: ");
         fflush(stdout);
      
      	//printf("Enter a command [date | who | df | exit]: ");
@@ -61,12 +77,15 @@ while(1) {
         // Receive response from server
         len = read(client_socket, buffer, sizeof(buffer));
         buffer[len] = '\0'; // Null-terminate the response
-        printf("Result of command:\n%s\n\n", buffer);
-}
+        
+        if (strcmp(buffer, "Keep_Alive") !=0) {
+        printf("Result of command:%s\n\n", buffer);
+       }
+      	last_command_time = time(NULL);
+      }  
     // Cleanup
     close(client_socket);
     printf("Client is finished\n");
-
     return 0;
 }
 
